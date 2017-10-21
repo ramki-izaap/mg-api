f941b0aa40b3905ff6dc4b650513e6d7724c2d18
ram-izaap <ram.izaap@gmail.com>
2017-10-21 13:41:28 +0530

    amend

--- application/controllers/Lead.php
+++ application/controllers/Lead.php
@@ -140,6 +140,80 @@ class Lead extends REST_Controller {
         $this->set_response($result, REST_Controller::HTTP_OK); 
     }
 
-    
+    public function followup_post()
+    {
+        print_r($_POST);die;
+        $user_id         = $this->post('id');
+
+        //echo $this->post('id');die;
+
+        $name = $this->post('name');
+        $sex = $this->post('sex');
+        $age = $this->post('age');
+        $mobile_no = $this->post('mobile_no');
+        $occupation = $this->post('occupation');
+        $email = $this->post('email');
+        $facebook = $this->post('facebook');
+        $fitness_goal = $this->post('fitness_goal');
+        $heard_from = $this->post('heard_from');
+        $refference = $this->post('refference');
+        $referred_by = $this->post('referred_by');
+        
+        $membership_id          = $this->post('membership_id');
+        $amount                 = $this->post('amount');
+        $expect_at              = $this->post('expect_at');
+
+        $co_ordinator           = $this->post('co_ordinator');
+        $co_ordinator_no        = $this->post('co_ordinator_no');
+
+        $comments               = $this->post('comments');
+
+        $data = array(
+                        'name' => $name, 
+                        'email' => $email,
+                        'facebook' => $facebook,
+                        'age' => $age, 
+                        'sex' => $sex, 
+                        'mobile_no' => $mobile_no,
+                        'occupation' => $occupation,
+                        'fitness_goal' => $fitness_goal,
+                        'heard_from' => $heard_from,
+                        'refference' => $refference,
+                        'referred_by' => $referred_by,
+
+                        'expect_at' => $expect_at,
+                        'comments' => $comments,
+                        'package' => $membership_id,
+                        'price' => $amount,
+                        'co_ordinator' => $co_ordinator,
+                        'co_ordinator_no' => $co_ordinator_no
+                        
+                    );
+
+        if( (int)$user_id )
+        {
+            $data['edited_on'] = date('Y-m-d H:i:s');
+
+            $this->db->where('id', $user_id);
+            $this->db->update('leads', $data);
+        }
+        else
+        {
+            
+            $data['created_on'] = date('Y-m-d H:i:s');
+
+            $this->db->insert('leads', $data);
+
+            $user_id = $this->db->insert_id();
+
+            //send Welcome SMS
+            $message = getSMSContent('welcome', array('name' => $name));
+            sendSMS(array($mobile_no), $message);
+        }
+
+        $result = array('status' => 'SUCCESS');
+
+        $this->set_response($result, REST_Controller::HTTP_OK); 
+    }
     
 }
